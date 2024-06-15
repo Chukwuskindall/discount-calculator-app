22f7903a4bbb45cb3920da71f91c0c74bed9e557
Chuks <nwachukwuemy@gmail.com>
2024-06-16 00:36:56 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import {View, Text, Modal} from 'react-native';
+import React from 'react';
+import Discount from './src/screens/Discount';
+import Switch from './src/screens/SwitchRN';
+import KeyboardAvoidRN from './src/screens/KeyboardAvoidRN';
+import ModalRN from './src/screens/ModalRN';
+
+const App = () => {
+  return <Discount />;
+};
+
+export default App;
